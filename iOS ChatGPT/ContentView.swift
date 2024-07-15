062dbdf2190b4564b82f6514e9eb7d3a2c4e3f4b
otamurod <safarovotamurod@gmail.com>
2024-07-15 19:57:55 +0500

    Initial Commit

---
+++ iOS ChatGPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS ChatGPT
+//
+//  Created by Otamurod Safarov on 15/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
