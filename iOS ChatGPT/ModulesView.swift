5b442eb35fdbce3919f68e2a6d03c2d82269a556
otamurod <safarovotamurod@gmail.com>
2024-07-15 20:03:20 +0500

    feat: Project Setup

---
+++ iOS ChatGPT/ModulesView.swift
@@ -0,0 +1,18 @@
+//
+//  ModulesView.swift
+//  iOS ChatGPT
+//
+//  Created by Otamurod Safarov on 15/07/24.
+//
+
+import SwiftUI
+
+struct ModulesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModulesView()
+}
